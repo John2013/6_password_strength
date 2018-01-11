37600daf28310d9d62ad94e46d62ddb69a47e1fd
John2013 <NaWasington@yandex.ru>
2018-01-11 20:07:30 +0300

    ball → score

--- password_strength.py
+++ password_strength.py
@@ -2,40 +2,40 @@ import re
 
 
 def get_password_strength(password):
-    length_ball = 1
-    digit_ball = 2
-    uppercase_ball = 2
-    lowercase_ball = 2
-    symbol_ball = 2
-    min_ball = 1
+    length_score = 1
+    digit_score = 2
+    uppercase_score = 2
+    lowercase_score = 2
+    symbol_score = 2
+    min_score = 1
 
     # calculating the length
-    length_rate = int(len(password) >= 8) * length_ball
+    length_rate = int(len(password) >= 8) * length_score
 
     # searching for digits
-    digit_rate = int(re.search(r"\d", password) is not None) * digit_ball
+    digit_rate = int(re.search(r"\d", password) is not None) * digit_score
 
     # searching for uppercase
     uppercase_rate = int(
         re.search(r"[A-Z]", password) is not None
-    ) * uppercase_ball
+    ) * uppercase_score
 
     # searching for lowercase
     lowercase_rate = int(
         re.search(r"[a-z]", password) is not None
-    ) * lowercase_ball
+    ) * lowercase_score
 
     # searching for symbols
     symbol_rate = int(
         re.search(
             r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password
         ) is not None
-    ) * symbol_ball
+    ) * symbol_score
 
     # overall result
     return (
         sum([length_rate, digit_rate, uppercase_rate, lowercase_rate,
-             symbol_rate]) + min_ball
+             symbol_rate]) + min_score
     )
 
 
