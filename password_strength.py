9aeb00e8b86d686631f97933b1ffbe0b38f69365
John2013 <NaWasington@yandex.ru>
2018-01-11 19:54:42 +0300

    basic functional

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,33 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    # calculating the length
+    length_rate = int(len(password) >= 8) * 2
+
+    # searching for digits
+    digit_rate = int(re.search(r"\d", password) is not None) * 2
+
+    # searching for uppercase
+    uppercase_rate = int(re.search(r"[A-Z]", password) is not None) * 2
+
+    # searching for lowercase
+    lowercase_rate = int(re.search(r"[a-z]", password) is not None) * 2
+
+    # searching for symbols
+    symbol_rate = int(
+        re.search(
+            r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password
+        ) is not None
+    ) * 2
+
+    # overall result
+    return (
+        sum([length_rate, digit_rate, uppercase_rate, lowercase_rate,
+             symbol_rate])
+    )
 
 
 if __name__ == '__main__':
-    pass
+    password = input("Enter your password: ")
+    print("Password strength: {}/10".format(get_password_strength(password)))
